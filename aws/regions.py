ce4d704836c19a4e81d7001d88aaa06061a55e0b
Hema-9676 <22jr1a4228@gmail.com>
2026-06-09 12:53:34 +0530

    created AWS authentication module structure

---
+++ aws/regions.py
@@ -0,0 +1,6 @@
+def get_regions():
+    return [
+        "us-east-1",
+        "us-west-2",
+        "ap-south-1"
+    ]
---
+++ aws/session_manager.py
@@ -0,0 +1,2 @@
+def create_session():
+    print("Creating AWS session...")
