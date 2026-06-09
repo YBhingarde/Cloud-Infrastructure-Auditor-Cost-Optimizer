fd076a83577f42cf2f2d7865663cea85b58b4bfc
Hema-9676 <22jr1a4228@gmail.com>
2026-06-09 12:55:56 +0530

    Added AWS region utility

--- aws/regions.py
+++ aws/regions.py
@@ -1,6 +1,9 @@
 def get_regions():
-    return [
+    regions = [
         "us-east-1",
         "us-west-2",
-        "ap-south-1"
+        "ap-south-1",
+        "eu-west-1"
     ]
+    
+    return regions
