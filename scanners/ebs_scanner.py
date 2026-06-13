596483371b31b9bd7be0c700bbdf8a6714351fc3
Ramya Thota <ramyathota1312@gmail.com>
2026-06-13 14:56:10 +0530

    Day 3: Added multi-region support for EBS and EIP scanners

--- scanners/ebs_scanner.py
+++ scanners/ebs_scanner.py
@@ -1,33 +1,24 @@
 import boto3
+from aws.regions import get_regions
 
-def get_ec2_client(region_name="us-east-1"):
-    return boto3.client(
-        "ec2",
-        region_name=region_name
-    )
 
-def get_available_volumes(region_name="us-east-1"):
-    ec2_client = get_ec2_client(region_name)
+def get_unattached_ebs_volumes():
+    unattached_volumes = []
 
-    response = ec2_client.describe_volumes(
-        Filters=[
-            {
-                "Name": "status",
-                "Values": ["available"]
-            }
-        ]
-    )
+    for region in get_regions():
+        ec2 = boto3.client("ec2", region_name=region)
 
-    volume_list = []
+        response = ec2.describe_volumes()
 
-    for volume in response["Volumes"]:
-        volume_list.append(
-            {
-                "VolumeId": volume["VolumeId"],
-                "Size": volume["Size"],
-                "State": volume["State"],
-                "Region": region_name
-            }
-        )
+        for volume in response["Volumes"]:
+            if volume["State"] == "available":
+                unattached_volumes.append(
+                    {
+                        "Region": region,
+                        "VolumeId": volume["VolumeId"],
+                        "Size": volume["Size"],
+                        "State": volume["State"]
+                    }
+                )
 
-    return volume_list
+    return unattached_volumes
--- scanners/eip_scanner.py
+++ scanners/eip_scanner.py
@@ -1,14 +1,23 @@
 import boto3
+from aws.regions import get_regions
 
 
-def get_ec2_client(region_name="us-east-1"):
-    """
-    Initialize EC2 client for a given AWS region.
-    """
+def get_unassociated_elastic_ips():
+    unused_ips = []
 
-    ec2_client = boto3.client(
-        "ec2",
-        region_name=region_name
-    )
+    for region in get_regions():
+        ec2 = boto3.client("ec2", region_name=region)
 
-    return ec2_client
+        response = ec2.describe_addresses()
+
+        for address in response["Addresses"]:
+            if "AssociationId" not in address:
+                unused_ips.append(
+                    {
+                        "Region": region,
+                        "PublicIp": address["PublicIp"],
+                        "AllocationId": address.get("AllocationId")
+                    }
+                )
+
+    return unused_ips
