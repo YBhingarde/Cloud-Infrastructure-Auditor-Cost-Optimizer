b0f5e2fb941c5f8d896d0f7d712c386713f101de
Ramya Thota <ramyathota1312@gmail.com>
2026-06-15 13:52:15 +0530

    Day 6: Added exception handling for AWS scanner failures

--- scanners/ebs_scanner.py
+++ scanners/ebs_scanner.py
@@ -1,5 +1,6 @@
 import boto3
 from aws.regions import get_regions
+from botocore.exceptions import ClientError
 
 
 def get_unattached_ebs_volumes():
@@ -8,19 +9,24 @@ def get_unattached_ebs_volumes():
     for region in get_regions():
         ec2 = boto3.client("ec2", region_name=region)
 
-        response = ec2.describe_volumes()
+        # THIS PART IS NEW
+        try:
+            response = ec2.describe_volumes()
 
-        for volume in response["Volumes"]:
-            if volume["State"] == "available":
-                unattached_volumes.append(
-                    {
-                        "resource_type": "EBS",
-                        "region": region,
-                        "resource_id": volume["VolumeId"],
-                        "size": volume["Size"],
-                        "status": volume["State"]
-                    }
-                )
+            for volume in response["Volumes"]:
+                if volume["State"] == "available":
+                    unattached_volumes.append(
+                        {
+                            "resource_type": "EBS",
+                            "region": region,
+                            "resource_id": volume["VolumeId"],
+                            "size": volume["Size"],
+                            "status": volume["State"]
+                        }
+                    )
+
+        except ClientError as e:
+            print(f"Error scanning {region}: {e}")
 
     return unattached_volumes
 
--- scanners/eip_scanner.py
+++ scanners/eip_scanner.py
@@ -1,5 +1,6 @@
 import boto3
 from aws.regions import get_regions
+from botocore.exceptions import ClientError
 
 
 def get_unassociated_elastic_ips():
@@ -8,19 +9,23 @@ def get_unassociated_elastic_ips():
     for region in get_regions():
         ec2 = boto3.client("ec2", region_name=region)
 
-        response = ec2.describe_addresses()
+        try:
+            response = ec2.describe_addresses()
 
-        for address in response["Addresses"]:
-            if "AssociationId" not in address:
-                unused_ips.append(
-                    {
-                        "resource_type": "ElasticIP",
-                        "region": region,
-                        "resource_id": address.get("AllocationId"),
-                        "public_ip": address["PublicIp"],
-                        "status": "unassociated"
-                    }
-                )
+            for address in response["Addresses"]:
+                if "AssociationId" not in address:
+                    unused_ips.append(
+                        {
+                            "resource_type": "ElasticIP",
+                            "region": region,
+                            "resource_id": address.get("AllocationId"),
+                            "public_ip": address["PublicIp"],
+                            "status": "unassociated"
+                        }
+                    )
+
+        except ClientError as e:
+            print(f"Error scanning {region}: {e}")
 
     return unused_ips
 
